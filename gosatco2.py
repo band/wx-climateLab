d4e0acdc0dfd830edd2e6ff20de0264678871168
William L. Anderson <band@acm.org>
2026-06-02 17:06:26 -0500

    print co2 and ch4 values

--- gosatco2.py
+++ gosatco2.py
@@ -7,10 +7,14 @@ logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the webpage to scrape
-url = 'https://www.gosat.nies.go.jp/en/recent-global-co2.html'
 
-def main():
+def scrape_print(url):
+
+    if 'co2' in url:
+        ghg = 'CO2'
+    elif 'ch4' in url:
+        ghg = 'CH4'
+        
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -26,8 +30,18 @@ def main():
         print(f"value: {pre.find('div', class_='values').text.strip()}")
         print("___________")
 
-    print(f"Latest {prelims[0].find('dd').text.strip()} monthly CO2 mean value: {prelims[0].find('div', class_='values').text.strip()} | trend value: {prelims[1].find('div', class_='values').text.strip()}")
-    print(f"CO2 growth in the past one year: {prelims[2].find('dd').text.strip()} : {prelims[2].find('div', class_='values').text.strip()}")
+    print(f"Latest {prelims[0].find('dd').text.strip()} monthly {ghg} mean value: {prelims[0].find('div', class_='values').text.strip()} | trend value: {prelims[1].find('div', class_='values').text.strip()}")
+    print(f"{ghg} growth in the past one year: {prelims[2].find('dd').text.strip()} : {prelims[2].find('div', class_='values').text.strip()}")
+
+def main():
+
+    # URL of the webpage to scrape
+    urlco2 = 'https://www.gosat.nies.go.jp/en/recent-global-co2.html'
+    urlch4 = 'https://www.gosat.nies.go.jp/en/recent-global-ch4.html'
+
+    scrape_print(urlco2)
+    scrape_print(urlch4)
+
 
 if __name__ == "__main__":
     exit(main())
