7070bbf60cd05b3940b76977ba33692a43a5a802
William L. Anderson <band@acm.org>
2026-06-03 17:30:08 -0500

    some guards added

--- gosatghgdata.py
+++ gosatghgdata.py
@@ -12,18 +12,20 @@ def scrape_print(url):
         ghg = 'CO2'
     elif 'ch4' in url:
         ghg = 'CH4'
-        
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+
+    soup = BeautifulSoup(requests.get(url, timeout=5).text, 'html.parser')
     prelims = soup.find_all("div", class_="preliminary")
 
-    print(f"Latest {prelims[0].find('dd').text.strip()} monthly {ghg} mean value: {prelims[0].find('div', class_='values').text.strip()} | trend value: {prelims[1].find('div', class_='values').text.strip()}")
-    print(f"{ghg} growth in the past one year: {prelims[2].find('dd').text.strip()} : {prelims[2].find('div', class_='values').text.strip()}\n")
+    print(f"Latest {prelims[0].find('dd').get_text(strip=True)} monthly {ghg} mean value: {prelims[0].find('div', class_='values').get_text(strip=True)} | trend value: {prelims[1].find('div', class_='values').get_text(strip=True)}")
+    print(f"{ghg} growth in the past one year: {prelims[2].find('dd').get_text(strip=True)} : {prelims[2].find('div', class_='values').get_text(strip=True)}\n")
 
 def main():
     urlco2 = 'https://www.gosat.nies.go.jp/en/recent-global-co2.html'
     urlch4 = 'https://www.gosat.nies.go.jp/en/recent-global-ch4.html'
     try:
+        print('Whole-atmosphere monthly mean CO2 concentration from Japan NIES GOSAT project:')
         scrape_print(urlco2)
+        print('Whole-atmosphere monthly mean CH4 concentration from Japan NIES GOSAT project:')
         scrape_print(urlch4)
     except Exception as e:
         logging.error(f"Error: {e}")
