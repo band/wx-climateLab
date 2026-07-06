a54993139441cd3fdc14ba7ca442262868245e29
William L. Anderson <band@acm.org>
2026-07-06 13:30:39 -0500

    add logging and a main fn

--- workbench/cdshdf_read.py
+++ workbench/cdshdf_read.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python3
 
+# setup logging
+import logging, os
+log_level = os.environ.get('LOGLEVEL', 'INFO').upper()
+
+logging.basicConfig(
+    level=getattr(logging, log_level, 'INFO'),
+    format="%(asctime)s - %(name)s - %(levelname)s: %(filename)s:%(lineno)d - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger('cds_hdf')
+
+import traceback
+
 import h5py
 
-testf1='/Volumes/sandisk4TB/clima_data/202412-C3S2-L4_GHRSST-SSTdepth-ISTskin-DMIOI-GLOB_ICDR1.0_monthly-v02.0-fv01.0.nc'
+def main():
+    try:
+        testf1='/Volumes/sandisk4TB/clima_data/62232c72/202412-C3S2-L4_GHRSST-SSTdepth-ISTskin-DMIOI-GLOB_ICDR1.0_monthly-v02.0-fv01.0.nc'
 
-hdft1 = h5py.File(testf1, 'r')
+        testf2='/Volumes/sandisk4TB/clima_data/793fc0b8/202201-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_ICDR3.0-v02.0-fv01.0.nc'
 
-print("the keys: ", hdft1.keys())
+        hdft1 = h5py.File(testf1, 'r')
+        logger.info(f"hdft1 keys: {hdft1.keys()}")
+
+        hdft2 = h5py.File(testf2, 'r')
+        logger.info(f"hdft2 keys: {hdft2.keys()}")
+    except Exception as e:
+        traceback.print_exc(e)
+        logging.error(f"Error: {e}")
+
+# run this script
+if __name__ == "__main__":
+    exit(main())
+
 
 
 
