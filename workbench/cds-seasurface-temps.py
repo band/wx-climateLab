995d8a94425f1fd2cb7fb29a227f124d5038f8f6
William L. Anderson <band@acm.org>
2026-07-06 16:03:07 -0500

    a few changes before putting these programs away

--- workbench/cds-seasurface-temps.py
+++ workbench/cds-seasurface-temps.py
@@ -1,32 +1,43 @@
+#!/usr/bin/env python3
+
+import logging, sys
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+import traceback
 from ecmwf.datastores import Client
 
-client = Client()
-result = client.check_authentication()
-print(f"auth check result: {result}")
+def main():
+    client = Client()
+    result = client.check_authentication()
+    print(f"auth check result: {result}")
 
-collection_id = "satellite-sea-surface-temperature"
+    collection_id = "satellite-sea-surface-temperature"
 
-request = {
-    "variable": "all",
-    "processinglevel": "level_4",
-    "sensor_on_satellite": "combined_product",
-    "version": "3_0",
-    "temporal_resolution": "monthly",
-    "year": [
-        "2016", "2017", "2018",
-        "2019", "2020", "2021",
-        "2022", "2023", "2024",
-        "2025", "2026"
-    ],
-    "month": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12"
-    ],
-    "data_format":"zip",
-    "download_format":"archived",
-}
+    request = {
+        "variable": "all",
+        "processinglevel": "level_4",
+        "sensor_on_satellite": "combined_product",
+        "version": "3_0",
+        "temporal_resolution": "monthly",
+        "year": [
+            "2016", "2017", "2018",
+            "2019", "2020", "2021",
+            "2022", "2023", "2024",
+            "2025", "2026"
+        ],
+        "month": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12"
+        ],
+        "data_format":"zip",
+        "download_format":"archived",
+    }
 
-client.retrieve(collection_id, request, target="seasurface_1.zip")
+    client.retrieve(collection_id, request, target="seasurface_1.zip")
+
+if __name__ == "__main__":
+    exit(main())
 
--- workbench/cdshdf_read.py
+++ workbench/cdshdf_read.py
@@ -21,11 +21,16 @@ def main():
 
         testf2='/Volumes/sandisk4TB/clima_data/793fc0b8/202201-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_ICDR3.0-v02.0-fv01.0.nc'
 
+        testf3='/Volumes/sandisk4TB/clima_data/999477d0/202605-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_ICDR3.0-v02.0-fv01.0.nc'
+
         hdft1 = h5py.File(testf1, 'r')
         logger.info(f"hdft1 keys: {hdft1.keys()}")
 
         hdft2 = h5py.File(testf2, 'r')
         logger.info(f"hdft2 keys: {hdft2.keys()}")
+
+        hdft3 = h5py.File(testf3, 'r')
+        logger.info(f"hdft3 keys: {hdft3.keys()}")
     except Exception as e:
         traceback.print_exc(e)
         logging.error(f"Error: {e}")
